25feac8997db2183d069f8b74f90abd0f66b9969
GJ <gao18066731853@163.com>
2020-09-01 11:07:56 +0800

    这是GJ的

--- src/main/java/com/gj/gaojie/Controller/testController.java
+++ src/main/java/com/gj/gaojie/Controller/testController.java
@@ -3,10 +3,12 @@ package com.gj.gaojie.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/didi")
 public class testController {
 
+	@GetMapping("/add")
 	public String add(){
+		//返回OK
 
 		return "OK";
 
