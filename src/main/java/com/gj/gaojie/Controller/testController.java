f1138365c79a373376d57d6516d56a968ebac151
GJ <gao18066731853@163.com>
2020-09-01 10:55:35 +0800

    这是GJ的

---
+++ src/main/java/com/gj/gaojie/Controller/testController.java
@@ -0,0 +1,14 @@
+package com.gj.gaojie.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+	public String add(){
+
+		return "OK";
+
+	}
+}
---
+++ src/main/java/com/gj/gaojie/GaojieApplication.java
@@ -0,0 +1,13 @@
+package com.gj.gaojie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaojieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaojieApplication.class, args);
+	}
+
+}
